a2f6ea19349e8618b466efc27ee109c54f63720f
Ivan Denchev <81493400+idencchev@users.noreply.github.com>
2021-12-19 22:37:49 +0200

    Update data.js

--- api/data.js
+++ api/data.js
@@ -4,35 +4,10 @@ export const login = api.login;
 export const register = api.register;
 export const logout = api.logout;
 
-const endpoints = {
-    cars: (query) => `/classes/carDB?where=${createQuery(query)}`
+export async function searchCar(query){
+    return api.get(`/classes/carDB?where={"RegNumber":"${query}"}`);
 }
 
 export async function create(data) {
     return await api.post('/classes/carDB', data);
-}
-
-export async function getCars(query) {
-
-    if (query) {
-        query = {
-            name: {
-                $text: {
-                    $search: query
-                }
-            }
-        }
-    }
-
-
-    return api.get(endpoints.cars());
-}
-
-export function createQuery(query) {
-    return encodeURIComponent(JSON.stringify(query))
-}
-
-
-export async function searchCar(regNumber){
-    return api.get(`/data/cars?where=RegNumber%3D${regNumber}`);
 }
