62d57b5a336959d0cd9943257ab12905a1bad2bb
Ivan Denchev <81493400+idencchev@users.noreply.github.com>
2021-12-19 19:51:01 +0200

    Update login.js

--- src/views/login.js
+++ src/views/login.js
@@ -47,7 +47,7 @@ async function onLogin(e) {
         }
         await login(form.get('username'), form.get('password'));
 
-        page.redirect('/');
+        page.redirect('/customer');
         updateUserNav();
     } catch (err) {
         //  alert(error);
