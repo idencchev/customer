6a71dbcb36cf4f52bfbf488dfd402b2f879ff2ec
Ivan Denchev <81493400+idencchev@users.noreply.github.com>
2021-12-19 08:49:09 +0200

    Update lib.js

--- src/lib.js
+++ src/lib.js
@@ -1,5 +1,5 @@
 import { html, render } from "../node_modules/lit-html/lit-html.js";
-import page from "../node_modules/page/page.mjs";
+import page from "https://unpkg.com/page@1.11.6/page.mjs";
 
 export {
     html,
