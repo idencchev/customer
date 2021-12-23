fb4dc3a8ccfda402b2f9c2ab0d213701e2436f18
Ivan Denchev <81493400+idencchev@users.noreply.github.com>
2021-12-23 03:27:40 +0200

    Update search.js

--- src/views/search.js
+++ src/views/search.js
@@ -1,17 +1,21 @@
 import { searchCar } from '../../api/data.js';
 import { html, page } from '../lib.js';
 
-const searchTemplate = (onSearch, data, params = '') => html`
+const searchTemplate = (onSearch, data, param = '') => html`
     <div class="content">
     
-        <input id="search-input" type="text" name="search" placeholder="For example: CU57ABC" .value=${params}>
+        <input id="search-input" type="text" name="search" placeholder="For example: CU57ABC" .value=${param}>
+        <select>
+            <option value="RegNumber">Register Number</option>
+            <option value="date">Date</option>
+        </select>
         <button @click=${onSearch} class="button-list">Search</button>
     
         <h2>Results:</h2>
         <div class="search-result">
-        ${data.results != undefined ? data.results.map(searchCart) : html`<p class="no-result">No result.</p>`}
-       
-            </div>
+            ${data.results.length ? data.results.map(searchCart) : html`<p class="no-result">No result.</p>`}
+    
+        </div>
     
     </div>`;
 
@@ -74,20 +78,21 @@ const searchCart = (data) => html`
 </form>`;
 
 export async function searchPage(ctx) {
-    const params = ctx.querystring.split('=')[1];
-    let data = [];
+    const params = ctx.querystring.split('&')[1];
+    let data = { results: [] };
 
-    if (params) {
-        data = await searchCar(params);
+    if (params != undefined) {
+        var [type, param] = params.split('=');
+        data = await searchCar(type, param);
     }
-    
-    ctx.render(searchTemplate(onSearch, data, params));
+    ctx.render(searchTemplate(onSearch, data, param));
 }
 
 async function onSearch() {
-    const search = document.getElementById('search-input').value
-    if (search) {
-        page.redirect(`/search?query=${encodeURIComponent(search)}`)
-    }
+    const search = document.getElementById('search-input').value.trim();
+    const select = document.querySelector('select').value;
 
+    if (search) {
+        page.redirect(`/search?query=type=${encodeURIComponent(`${select}&${select}=${search}`)}`);
+    }
 }
