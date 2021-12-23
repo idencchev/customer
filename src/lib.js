bdf2cae34ac91a27d5e84dac50dd27fcc81f519c
Ivan Denchev <81493400+idencchev@users.noreply.github.com>
2021-12-23 21:48:00 +0200

    Delete lib.js

--- src/lib.js
+++
@@ -1,8 +0,0 @@
-import { html, render } from "https://unpkg.com/lit-html@2.0.2/lit-html.js";
-import page from "https://unpkg.com/page@1.11.6/page.mjs";
-
-export {
-    html,
-    render,
-    page
-}
