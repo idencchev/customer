d0b66ef1ba35d9dc73eda30c0ed61b8913d074c4
Ivan Denchev <81493400+idencchev@users.noreply.github.com>
2021-12-24 20:05:38 +0200

    Update data.js

--- api/data.js
+++ api/data.js
@@ -11,3 +11,15 @@ export async function searchCar(type, query){
 export async function create(data) {
     return await api.post('/classes/carDB', data);
 }
+
+export async function deleteCar(id) {
+    return await api.del(`/classes/carDB/${id}`);
+}
+
+export async function editCar(id, data) {
+    return await api.put(`/classes/carDB/${id}`, data);
+}
+
+export async function getCar(id) {
+    return await api.get(`/classes/carDB/${id}`);
+}
